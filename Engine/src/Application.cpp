9d351aadd78170e0e1aea8fa2467db92fde000f0
Ashu <ashutoshvishwakarma208@gmail.com>
2023-05-05 18:30:02 +0530

    Added for rectangle

--- Engine/src/Application.cpp
+++ Engine/src/Application.cpp
@@ -1,30 +1,66 @@
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <time.h>
+#include <fstream>
+
+
+#if 1
+#define LOG(x) std::cout<<x<<std::endl;
+#else
+#define LOG(x)
+#endif
+
+
+
+
+/*
+Function to load files
+@param path: char array to the path of file
+@param str: string reference to get the output[
+*/
+void loadFile(const char* path, std::string& str) {
+    std::ifstream in;
+    in.open(path);
+
+    if (!in.is_open()) {
+        LOG("Failed to open " + (std::string)path);
+        return;
+    }
+    str = "";
+    while (!in.eof()) {
+        char temp[500];
+        in.getline(temp, 500);
+        str += (std::string)temp + '\n';
+    }
+    str += '\0';
+}
+
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);      //adjusts images on resize
 void processInput(GLFWwindow* window);          //handles inputs
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
 
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-" FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
-"}\n";
+const char* vertexShaderSource;
+const char* fragmentShaderSource;
+
+
+void shaderInit(const char* vs,  const char* fs) {
+    std::string str="";
+    loadFile(vs, str);
+    vertexShaderSource = str.c_str();
+    std::string str1 = "";
+    loadFile(fs, str1);
+    fragmentShaderSource = str1.c_str();
+   
+}
+
 
 
 int main()
-{
+{   
+    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -33,16 +69,20 @@ int main()
     GLFWwindow* window = glfwCreateWindow(800, 600, "Ashutosh", NULL, NULL);
 
     if (window == NULL) {
-        std::cout << "Failed to open" << std::endl;
+        LOG("Failed to open");
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
 
+
+    shaderInit("../Engine/src/Shaders/vertex.glsl", "../Engine/src/Shaders/frag.glsl");
+
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        LOG("Failed to initialize GLAD");
         return -1;
     }
 
@@ -51,22 +91,43 @@ int main()
     
    
     float vertices[] = {
-        -0.5f,-0.5f,0.0f,
-         0.0f,0.5f,0.0f,
-         0.5f,-0.5f,0.0f
-
+      0.5f,  0.5f, 0.0f,  // top right
+      0.5f, -0.5f, 0.0f,  // bottom right
+     -0.5f, -0.5f, 0.0f,  // bottom left
+     -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
     };
 
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
+
+
 
     //Creasting a vertex shader
     unsigned int vertexShader;
@@ -82,7 +143,8 @@ int main()
     if (!success)
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        LOG("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+        LOG(infoLog);
     }
 
     //Creating fragment shader
@@ -94,7 +156,7 @@ int main()
     if (!success)
     {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        LOG("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
     }
 
     //linking the shaders
@@ -110,35 +172,38 @@ int main()
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        LOG("ERROR::SHADER::PROGRAM::LINKING_FAILED\n");
+        LOG(infoLog);
     }
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
 
 
-
-
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     while (!glfwWindowShouldClose(window))
     {
         clock_t t = clock();
-        glClearColor(0.1f, 0.1f, 0.0f, 0.5f);
+        glClearColor(0.1f, 0.1f, 0.1f, 0.5f);
         glClear(GL_COLOR_BUFFER_BIT);
         processInput(window);
+
+
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
         t = clock() - t;
-        std::cout << CLOCKS_PER_SEC/((float)t)<< std::endl;
+        //LOG(((float)CLOCKS_PER_SEC)/t);
+        
     }
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
---
+++ Engine/src/Shaders/frag.glsl
@@ -0,0 +1,6 @@
+#version 330 core
+out vec4 FragColor;
+void main()
+{
+	FragColor = vec4(0.3843, 0.9098, 0.0588, 0.934);
+}
