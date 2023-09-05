e3fd7172dcf82338cd6b2bd87fc88c30c325d1e6
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2023-09-05 21:20:38 +0530

    import glm

--- Engine/src/Shaders/frag.glsl
+++ Engine/src/Shaders/frag.glsl
@@ -2,7 +2,7 @@
 #extension GL_ARB_explicit_uniform_location : enable
 #extension GL_ARB_explicit_attrib_location : enable
 out vec4 FragColor;
-uniform vec4 c;
+uniform vec4 c;        
 void main()
 {
 	FragColor = c;
--- Engine/src/Shaders/vertex.glsl
+++ Engine/src/Shaders/vertex.glsl
@@ -2,7 +2,9 @@
 #extension GL_ARB_explicit_uniform_location : enable
 #extension GL_ARB_explicit_attrib_location : enable
 layout (location = 0) in vec3 aPos;
+uniform mat4 transform;
 void main()
 {
-	gl_Position = vec4(aPos,1.0);
+	gl_Position = transform*vec4(aPos,1.0);
+	
 }
